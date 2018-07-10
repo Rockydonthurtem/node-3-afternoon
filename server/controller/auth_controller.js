116e0ed086bc655d8e9efa03b3f5db0e472dc0aa
Rockydonthurtem <charrisq@hotmail.com>
2018-07-10 18:06:19 -0500

    wk4d2

--- server/controller/auth_controller.js
+++ server/controller/auth_controller.js
@@ -2,9 +2,37 @@ const swag = require("../models/users")
 
 let id = 1
 
-module.export = {
+module.exports = {
     login: (req, res, next) => {
+        const { session } = req
 
+        // const { username, password } = req.body;
+        const user = users.find(user => user.username === username && user.password === password)
+        // if (users[i] === { username, password })
+        if (user) {
+            session.user.username = user.name;
+            res.status(200).send(session.user)
+        } else {
+            res.status(500).send("No Bueno")
+        }
+        // user = session.user.username;
+        // res.status(200).send(user).catch.staus(500).send('err')
+
+    },
+    register: (req, res, next) => {
+        const { username, password } = req.body;
+        users.push({ id, username, password }); id++;
+        res.status(200).send()
+
+    },
+    signout: (req, res, next) => {
+        const { session } = req;
+        session.destroy();
+        res.status(200).send(req.session)
+    },
+
+    getUser: (req, res, next) => {
+        const { session } = req
+        res.status(200).send(session.user)
     }
-
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+
 require('dotenv').config();
 
 const { checkForSession } = require("./middleware/checkForSession")
+const auth_controller = require('./controller/auth_controller')
+const cart_controller = require('./controller/cart_controller')
 
 const swag_controller = require("./controller/swag_controller")
+const search_controller = require("./controller/search_controller")
 
 const app = express();
+app.use(express.static(`${__dirname}/ build`))
 
 app.use(bodyParser.json());
 app.use(session({
@@ -17,11 +22,21 @@ app.use(session({
 }));
 // app.use(checkForSession());
 console.log(checkForSession)
-
+console.log(auth_controller)
 app.get('/api/swag/', swag_controller.read)
 
-const port = 3000
-// const port = process.env.PORT || 3000;
+// app.post('/api/login', auth_controller.login)
+app.post('/api/register', auth_controller.register)
+app.post('/api/signout', auth_controller.signout)
+app.get('/api/user', auth_controller.getUser)
+
+
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+
+app.get('/api/search', search_controller.search)
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => { console.log(`Server listening on port ${port}.`); });
 
-secret: "ff";
